52b68a0a0ce39298187b64c621ffc0dd79f1ef83
Sharon <sharon.m0626@gmail.com>
2023-09-22 15:30:31 -0700

    Add next.config.js and .nojekyll

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath: "/sharonxhma.github.io",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
